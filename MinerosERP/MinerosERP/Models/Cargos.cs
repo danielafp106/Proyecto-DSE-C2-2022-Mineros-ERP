831df9b6278e90efdce2ee024ca2dbfe446845ea
danielwilfredo <xvwilito11@gmail.com>
2022-10-31 22:32:55 -0600

    empleados guarda, lista, falta eliminar y editar

--- MinerosERP/MinerosERP/Models/Cargos.cs
+++ MinerosERP/MinerosERP/Models/Cargos.cs
@@ -3,7 +3,7 @@
     public class Cargos
     {
         //Cargos de empleados en la empresa
-        public int id_cargo_empleado { get; set; }
-        public string cargo_empleado { get; set; }
+        public int id_cargos_empleado { get; set; }
+        public string cargos_empleado { get; set; }
     }
 }
